bb8b4d65f9cad5dc63e5e87bfcdd565f9915ccdc
Tianyao Ma <tianyao.ma333@gmail.com>
2021-07-18 17:50:34 -0400

    code completion

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../styles/App.css';
+import React from 'react';
+import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+      <div className="App">
+        <Header />
+          <Main />
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
+
